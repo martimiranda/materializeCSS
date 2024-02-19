a09bbebc16ae64f5a9d22ace81e4357e881adf2c
martimiranda <martimiranda2356@gmail.com>
2024-02-19 20:37:16 +0100

    last commit

--- www/js/init.js
+++ www/js/init.js
@@ -1,17 +1,24 @@
-    (function($){
-      $(function(){
-        $('.sidenav').sidenav();
-      }); // end of document ready
-    })(jQuery); // end of jQuery name space
-     
-     
-    document.addEventListener('deviceready', onDeviceReady, false);
-     
-    function onDeviceReady() {
-        // Cordova is now initialized. Have fun!
-     
-        console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-        //document.getElementById('deviceready').classList.add('ready');
-    }
+(function ($) {
+  $(function () {
+    // Inicializar la barra lateral
+    $('.sidenav').sidenav();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 
+document.addEventListener('deviceready', onDeviceReady, false);
 
+function onDeviceReady() {
+  // Cordova está ahora inicializado. ¡Diviértete!
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+  //document.getElementById('deviceready').classList.add('ready');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Inicializar la barra lateral nuevamente (por si acaso)
+  $('.sidenav').sidenav();
+
+  // Inicializar las tabs
+  var options = { "swipeable": true };
+  var el = document.querySelector('#tabs-swipe-demo');
+  var instance = M.Tabs.init(el, options);
+});
